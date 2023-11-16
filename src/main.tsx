880422ab6577d4e93d17099f7a00410bb926e1ea
Camino Losada <camino.losada.alonso@gmail.com>
2023-11-16 17:29:25 +0100

    Add GlobalStyles

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
+import "@fontsource/montserrat";
 import { store } from "./store";
 import { Router } from "../src/router/WebRouter.routes";
+import GlobalStyle from "./styles/GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <Router />
+      <GlobalStyle />
     </Provider>
   </React.StrictMode>
 );
